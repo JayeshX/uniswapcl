3a4784aaf3aa2eaf8ac6ad7a50568a33228e87bb
JayeshX <jayeshkirtane218@gmail.com>
2024-03-18 20:02:25 +0530

    sepolia trial

--- src/config.js
+++ src/config.js
@@ -1,11 +1,23 @@
-import { Goerli } from "@usedapp/core";
+// import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "0x756D6E08Be7C15f6045e87CeA0D9d03bfba87592";
+// export const ROUTER_ADDRESS = "0x756D6E08Be7C15f6045e87CeA0D9d03bfba87592";
+
+// export const DAPP_CONFIG = {
+//   readOnlyChainId: Goerli.chainId,
+//   readOnlyUrls: {
+//     [Goerli.chainId]:
+//       "https://eth-goerli.g.alchemy.com/v2/PuHnBqS1NQeEd-GDk37LpGNN3YxnthQv",
+//   },
+// };
+
+import { Sepolia } from "@usedapp/core";
+
+export const ROUTER_ADDRESS = "0x361280f97263cbebd0874596ec64d49898177aeec3b4f9116f8f4e5b4ee7fab"; 
 
 export const DAPP_CONFIG = {
-  readOnlyChainId: Goerli.chainId,
+  readOnlyChainId: Sepolia.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]:
-      "https://eth-goerli.g.alchemy.com/v2/PuHnBqS1NQeEd-GDk37LpGNN3YxnthQv",
+    [Sepolia.chainId]: "https://eth-sepolia.g.alchemy.com/v2/-VMN3vAg4pjN8X3kuYJJczE6BNMq7CdG",
+     
   },
 };
